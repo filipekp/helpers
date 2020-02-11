1a41c105c4f4589b671845cd9776cff2ef811efa
Pavel Filípek <filipek@proclient.cz>
2020-02-11 14:58:30 +0100

    -uprava loggeru pro praci s archivaci souborů v1

--- src/helpers/Logger.php
+++ src/helpers/Logger.php
@@ -2,8 +2,9 @@
   
   namespace PF\helpers;
   
+  use Tracy\Debugger;
   use ZipArchive;
-
+  
   /**
    * Třída Logger.
    *
@@ -13,41 +14,41 @@
    */
   class Logger
   {
-  
+    
     const LOG_TYPE_JSON = 'json';
     const LOG_TYPE_TXT  = 'txt';
     const LOG_TYPE_LOG  = 'log';
-  
+    
     private $allowedTypes = [
       self::LOG_TYPE_JSON,
       self::LOG_TYPE_TXT,
       self::LOG_TYPE_LOG,
     ];
-  
+    
     private $fileContent = NULL;
     private $fileContentArray = [];
     private $dir = NULL;
     private $fileName = NULL;
     private $prefix = NULL;
     private $type = self::LOG_TYPE_JSON;
-  
+    
     private $timeOld           = '7 days';
     private $archiveNoDelete   = FALSE;
     private $archiveFolderName = 'archive';
-  
+    
     private $changed = FALSE;
     private $new = TRUE;
-  
+    
     public function __construct($prefix, $fileName, $dir = NULL, $type = self::LOG_TYPE_JSON) {
       $this->prefix = $prefix;
       $this->fileName = $fileName;
-      $this->dir = rtrim(((is_null($dir)) ? __DIR__ . '/log/' : $dir), '/') . '/';
+      $this->dir = rtrim(((is_null($dir)) ? __DIR__ . DIRECTORY_SEPARATOR . 'log' . DIRECTORY_SEPARATOR : $dir), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
       $this->type = $type;
-    
+      
       $this->checkAllowedType();
       $this->load();
     }
-  
+    
     /**
      * Vrátí typ souboru.
      * @return mixed|string
@@ -55,7 +56,7 @@
     private function getType() {
       return $this->type;
     }
-  
+    
     /**
      * Zkontroluje zda je soubor povoleného typu.
      * @return bool
@@ -63,7 +64,7 @@
     private function checkAllowedType() {
       return in_array($this->getType(), $this->allowedTypes);
     }
-  
+    
     /**
      * Vrátí celou cestu k souboru.
      * @return string
@@ -71,7 +72,7 @@
     public function getFilePath() {
       return $this->dir . $this->prefix . $this->fileName . '.' . $this->type;
     }
-  
+    
     /**
      * Vrátí zda soubor existuje či neexistuje.
      * @return bool
@@ -79,7 +80,7 @@
     private function exists() {
       return file_exists($this->getFilePath());
     }
-  
+    
     /**
      * Vytvoří rekurzivně logovací adresář, pokud ještě neexistuje.
      */
@@ -88,7 +89,7 @@
         File::mkDir($this->dir, TRUE);
       }
     }
-  
+    
     /**
      * Upraví řádek do správného tvaru.
      *
@@ -100,20 +101,20 @@
      */
     private function prepareLine($content, $addDate = TRUE) {
       $data = [];
-    
+      
       if (is_array($content)) {
         $data = $content;
       } elseif (is_string($content)) {
         $data = [$content];
       }
-    
+      
       if ($addDate) {
         $data['date'] = (new \DateTime('now'))->format('Y-m-d H:i:s.u');
       }
-    
+      
       return $data;
     }
-  
+    
     /**
      * Přidá záznam na konec souboru.
      *
@@ -126,10 +127,10 @@
     public function append($content, $addDate = TRUE) {
       $this->fileContentArray[] = $this->prepareLine($content, $addDate);
       $this->changed = TRUE;
-    
+      
       return $this;
     }
-  
+    
     /**
      * Přidá záznam na konec souboru a uloží.
      *
@@ -141,10 +142,10 @@
      */
     public function appendStore($content, $addDate = TRUE) {
       $this->append($content, $addDate)->store();
-    
+      
       return $this;
     }
-  
+    
     /**
      * Přidá záznam na začátek souboru.
      *
@@ -157,10 +158,10 @@
     public function prepend($content, $addDate = TRUE) {
       $this->fileContentArray = array_merge([$this->prepareLine($content, $addDate)], (array)$this->fileContentArray);
       $this->changed = TRUE;
-    
+      
       return $this;
     }
-  
+    
     /**
      * Přidá záznam na začátek souboru a uloží.
      *
@@ -172,10 +173,10 @@
      */
     public function prependStore($content, $addDate = TRUE) {
       $this->prepend($content, $addDate)->store();
-    
+      
       return $this;
     }
-  
+    
     /**
      * Nastaví obsah souboru. Lze použít jen pokud se jedná o nový soubor a není typu JSON, jinak použíjte metody
      * {@link \prosys\Logger::append} nebo {@link \prosys\Logger::prepend}.
@@ -188,7 +189,7 @@
         $this->changed = TRUE;
       }
     }
-  
+    
     /**
      * Rozparsuje data a uloží do pole.
      */
@@ -199,7 +200,7 @@
           case self::LOG_TYPE_LOG:
             $this->fileContentArray = (array)explode(PHP_EOL, $this->fileContent);
             break;
-        
+          
           case self::LOG_TYPE_JSON:
           default:
             $this->fileContentArray = json_decode($this->fileContent, TRUE);
@@ -207,7 +208,7 @@
         }
       }
     }
-  
+    
     /**
      * Načte obsah souboru do fileContent.
      * @return $this
@@ -216,13 +217,13 @@
       if ($this->exists()) {
         $this->fileContent = file_get_contents($this->getFilePath());
         $this->new = FALSE;
-      
+        
         $this->parseData();
       }
-    
+      
       return $this;
     }
-  
+    
     /**
      * Uloží log do souboru.
      *
@@ -233,14 +234,14 @@
      */
     public function store() {
       $storeRes = NULL;
-    
+      
       if ($this->changed) {
         $this->createDir();
-      
+        
         if ($this->exists() && $this->new) {
           $this->fileName = MyString::seoTypeConversion($this->fileName . '_' . date('Ymd_His'));
         }
-      
+        
         switch ($this->getType()) {
           case self::LOG_TYPE_TXT:
           case self::LOG_TYPE_LOG:
@@ -255,52 +256,53 @@
                 }
                 $res .= var_export($row, TRUE);
               }
-            
+              
               return $res;
             }, $this->fileContentArray));
             break;
-        
+          
           case self::LOG_TYPE_JSON:
           default:
             $string = json_encode($this->fileContentArray, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
             break;
         }
-      
+        
         if (($storeRes = (bool)file_put_contents($this->getFilePath(), $string))) {
           if (file_exists($this->getFilePath())) {
             $oldUmask = umask(0);
             chmod($this->getFilePath(), 0777);
             umask($oldUmask);
           }
-        
+          
           $this->fileContent = $string;
           $this->changed = FALSE;
           $this->setNew(FALSE);
         }
       }
-    
+      
       $this->deleteOld();
-    
+      
       return $storeRes;
     }
-  
+    
     /**
      * Smaže staré soubory.
      */
     public function deleteOld() {
       $files = glob($this->dir . $this->prefix . '*.' . $this->type);
       $timeToCompare = strtotime('-' . $this->timeOld);
-    
+      
       $processArchive = FALSE;
       foreach ($files as $file) {
         if (is_file($file) && filemtime($file) < $timeToCompare) {
           if ($this->archiveNoDelete) {
             $filename = basename($file);
-            $dir = trim(dirname($filename), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . $this->archiveFolderName . DIRECTORY_SEPARATOR;
+            $dir = trim(dirname($file), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . $this->archiveFolderName . DIRECTORY_SEPARATOR;
             if (!is_dir($dir)) {
               File::mkDir($dir);
             }
-            $processArchive = $processArchive || rename($file, $dir . $filename);
+            
+            $processArchive = rename($file, $dir . $filename) || $processArchive;
           } else {
             unlink($file);
           }
@@ -311,15 +313,15 @@
         $this->processArchiveFolder();
       }
     }
-  
+    
     /**
      * Zabali soubory v archivu
      */
     private function processArchiveFolder() {
       $archives = [];
-      $folder = $this->dir . DIRECTORY_SEPARATOR . $this->archiveFolderName . DIRECTORY_SEPARATOR;
+      $folder = $this->dir . $this->archiveFolderName . DIRECTORY_SEPARATOR;
       
-      $files = glob($folder . $this->prefix . '*.');
+      $files = glob($folder . $this->prefix . '*');
       foreach ($files as $file) {
         $file = array_merge([
           'filePath' => $file,
@@ -335,22 +337,27 @@
             'files' => [],
           ];
         }
-  
+        
         $archives[$zipName]['files'][] = $file;
       }
       
       foreach ($archives as $zipFileName => $zipData) {
         $zip = new \ZipArchive();
+        $toDeleteFiles = [];
         $zip->open($folder . $zipFileName . '.zip', ZipArchive::CREATE);
         foreach ($zipData['files'] as $archiveFile) {
-          if ($zip->addFile($archiveFile['filePath'])) {
-            unlink($archiveFile['filePath']);
+          if ($zip->addFile($archiveFile['filePath'], str_replace($folder, '', $archiveFile['filePath']))) {
+            $toDeleteFiles[] = $archiveFile['filePath'];
           }
         }
         $zip->close();
+        
+        foreach ($toDeleteFiles as $toDeleteFile) {
+          unlink($toDeleteFile);
+        }
       }
     }
-  
+    
     /**
      * Vrátí zda byl soubor změněn či nikoli.
      *
@@ -359,7 +366,7 @@
     public function wasChanged() {
       return $this->changed;
     }
-  
+    
     /**
      * Zjistí zda je soubor nový.
      *
@@ -368,7 +375,7 @@
     public function isNew() {
       return $this->new;
     }
-  
+    
     /**
      * Nastaví zda se jedná o nový či starý soubor.
      *
@@ -377,7 +384,7 @@
     public function setNew($bool = TRUE) {
       $this->new = $bool;
     }
-  
+    
     /**
      * Vrátí instanci třídy Logger a načte obsah souboru pokud existuje.
      *
@@ -390,10 +397,10 @@
      */
     public static function create($prefix, $fileName, $dir = NULL, $type = self::LOG_TYPE_JSON) {
       $logger = new self($prefix, $fileName, $dir, $type);
-    
+      
       return $logger;
     }
-  
+    
     /**
      * Nastaví dobu kešování souborů v historii.
      *
@@ -402,13 +409,13 @@
     public function setCacheTime($stringTime = '7 days') {
       $this->timeOld = $stringTime;
     }
-  
+    
     public function archiveNoDelete($archiveNoDelete = TRUE) {
       $this->archiveNoDelete = $archiveNoDelete;
       
       return $this;
     }
-  
+    
     /**
      * Vrátí obsah souboru.
      *
